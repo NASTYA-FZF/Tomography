3c884e800c899999bb457c57c4a8f2f7080a994c
NASTYA_FZF <2002marev@gmail.com>
2024-11-24 13:58:33 +0300

    Добавьте файлы проекта.

---
+++ Tomography/Tomography.h
@@ -0,0 +1,32 @@
+﻿
+// Tomography.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CTomographyApp:
+// Сведения о реализации этого класса: Tomography.cpp
+//
+
+class CTomographyApp : public CWinApp
+{
+public:
+	CTomographyApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTomographyApp theApp;
---
+++ Tomography/TomographyDlg.h
@@ -0,0 +1,33 @@
+﻿
+// TomographyDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CTomographyDlg
+class CTomographyDlg : public CDialogEx
+{
+// Создание
+public:
+	CTomographyDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TOMOGRAPHY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
